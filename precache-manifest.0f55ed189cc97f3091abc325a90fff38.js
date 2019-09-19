dbcaa45aa9b67ddaf2049f9ea34b4a02da72eaee
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-09-19 16:02:05 -0400

    Updates

---
+++ precache-manifest.0f55ed189cc97f3091abc325a90fff38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4257e180646cb983dbb27c75661cce5",
+    "url": "/wannab3d3v3lop3r/react-api-drill/index.html"
+  },
+  {
+    "revision": "7ebfefab5ecaea062078",
+    "url": "/wannab3d3v3lop3r/react-api-drill/static/css/main.2d98336c.chunk.css"
+  },
+  {
+    "revision": "97a41fe9981b90c2a1f8",
+    "url": "/wannab3d3v3lop3r/react-api-drill/static/js/2.94b5abd5.chunk.js"
+  },
+  {
+    "revision": "7ebfefab5ecaea062078",
+    "url": "/wannab3d3v3lop3r/react-api-drill/static/js/main.29fa9ebd.chunk.js"
+  },
+  {
+    "revision": "a0773cdfef36bd1bfca0",
+    "url": "/wannab3d3v3lop3r/react-api-drill/static/js/runtime~main.b69da1ba.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wannab3d3v3lop3r/react-api-drill/precache-manifest.0f55ed189cc97f3091abc325a90fff38.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wannab3d3v3lop3r/react-api-drill/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
