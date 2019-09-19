0a72ae0cc0834696ba1e5956c66a449612102dfa
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-09-19 16:00:19 -0400

    edit css for better responsive display on book info

--- src/Bookinfo/Bookinfo.js
+++ src/Bookinfo/Bookinfo.js
@@ -5,7 +5,9 @@ export class Bookinfo extends Component {
     render() {
         return (
             <div className='lists'>
-                <a target={`_blank`} href={`${this.props.imageUrl}`}><img alt={`${this.props.title}`} src={`${this.props.image}`}/></a>
+                <div class="img">
+                    <a target={`_blank`} href={`${this.props.imageUrl}`}><img alt={`${this.props.title}`} src={`${this.props.image}`}/></a>
+                </div>
                 <div className='lists-text-container'>
                     <h1>{this.props.title}</h1>
                     <p>Author: {this.props.author}</p>
